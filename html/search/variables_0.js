5ff8623f73713e4cebb689e972ad48f84831794d
David Malone <dmalone7@cs.utexas.edu>
2016-02-04 21:33:52 -0600

    fixed html file after following instructions in RunCollatz.c++

---
+++ html/search/variables_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['cache',['cache',['../Collatz_8c_09_09.html#a95404fd9917eb5df19dd437f4c06fadc',1,'cache():&#160;Collatz.c++'],['../SphereCollatz_8c_09_09.html#a95404fd9917eb5df19dd437f4c06fadc',1,'cache():&#160;SphereCollatz.c++']]]
+];
---
+++ html/search/variables_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['values',['values',['../Collatz_8c_09_09.html#a0a03fc1561b37bc64de7fbed91433ff8',1,'values():&#160;Collatz.c++'],['../SphereCollatz_8c_09_09.html#a0a03fc1561b37bc64de7fbed91433ff8',1,'values():&#160;SphereCollatz.c++']]]
+];
